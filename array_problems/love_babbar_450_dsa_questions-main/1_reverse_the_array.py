b1daa0bb45fc6f99ff46c2e4a6fc3f78b9bf2a1d
NifulIslam <78269383+NifulIslam@users.noreply.github.com>
2021-02-25 20:30:01 +0600

    Delete array_problems directory

--- array_problems/love_babbar_450_dsa_questions-main/1_reverse_the_array.py
+++
@@ -1,9 +0,0 @@
-def reverse_arr(arr):
-    for i in range((len(arr)-1)//2):
-        arr[i],arr[len(arr)-1-i]= arr[len(arr)-1-i], arr[i]
-        
-import numpy as np
-arr= np.array(list(map(int, input().split())))
-reverse_arr(arr)
-print(arr)
-
--- array_problems/love_babbar_450_dsa_questions-main/5_move_all_negative.py
+++
@@ -1,5 +0,0 @@
-    def bubble_sort(self):
-        for i in range(len(self.arr)):
-            for j in range(i+1, len(self.arr)):
-                if(arr[i]>arr[j]):
-                    arr[i],arr[j]=arr[j],arr[i]
